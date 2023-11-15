0b06ebf41a1a782c43c9b5479e0683fb0426d79c
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2023-11-15 11:46:41 +0400

    Setup base

---
+++ README.md
@@ -0,0 +1,19 @@
+# RiotJS Vite Setup
+
+### Quickstart
+
+Install npm packages:
+```bash
+npm install
+```
+
+Start development server:
+```bash
+npm run dev
+```
+
+Build the front for production into a `dist` directory.
+```bash
+npm run build
+```
+
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import riot from 'rollup-plugin-riot'
+
+/**
+ * Vite configuration: https://vitejs.dev/config/
+ */
+
+export default defineConfig({
+  root       : process.cwd() + '/client',
+  plugins    : [riot()],
+  build: { 
+    minify       : 'esbuild', /** https://vitejs.dev/config/build-options.html#build-target */
+    target       : 'esnext' /** https://vitejs.dev/config/build-options.html#build-target */
+  }
+})
